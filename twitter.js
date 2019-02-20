fa0c5c80659461facf27eaaeefe033385a53f28e
laizaraujo8 <laiz.araujo8@gmail.com>
2019-02-20 15:56:42 -0300

    versao 1

--- twitter.js
+++ twitter.js
@@ -1,33 +1,27 @@
 
-function twittar() {
-  let inserir = document.getElementById("texto").value;
-  document.getElementById("texto2").value = inserir;
-}
+document.getElementById("texto").addEventListener("keyup", validar);
+document.getElementById("botaoEnviar").addEventListener("click", twittar);
+
 
 function validar() {
   let inserir = document.getElementById("texto").value;
-  if (inserir.length > 0 && inserir.length <= 140)  {
-  document.getElementById("botaoEnviar").removeAttribute("disabled");
+  console.log(inserir);
+  if (inserir.length > 0 && inserir.length <= 140) {
+    document.getElementById("botaoEnviar").removeAttribute("disabled");
   }
 
-  if (inserir.length===0 || inserir.length>140){
-    document.getElementById("botaoEnviar").setAttribute("disabled","disabled"); 
+  if (inserir.length === 0 || inserir.length > 140) {
+    document.getElementById("botaoEnviar").setAttribute("disabled", "disabled");
   }
+console.log("validar")
 }
-
-function caixasTwitte(){
-  let inserir = document.createElement("li");
-  novalista.textcontent=texto.value;
-}
-
-function myFunction() {
-  alert('Ok')
-}
-
-function myFunction() {
-  alert('agr vc chamou a função')
-}
-
 function twittar() {
-  document.getElementById("inserir").innerHTML = "texto";
+  let inserir = document.getElementById("texto").value;
+  var p = document.createElement('p');
+  let pText =  document.createTextNode(inserir);
+  p.appendChild(pText);
+  document.getElementById("div-p").appendChild(p);
+  
+
+  //document.getElementById("enviado").innerHTML=inserir;
 }
